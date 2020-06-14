e1685c1ed10ee3b70ca29a1bcd133359d2623fb1
nikas <nikasampetova@gmail.com>
2020-06-15 01:13:52 +0300

    7 задание - начало (открытие карточки)

--- js/main.js
+++ js/main.js
@@ -1,21 +1,59 @@
 'use strict';
 (function () {
+  var filteredPins = [];
+  var indexClickedPin;
+  var pinClicked;
+
   var turnOnPage = function () {
     if (!window.utils.isMapOn) {
       window.utils.isMapOn = true;
       window.map.turnOn();
       window.form.turnOn();
+      filteredPins = window.map.filterPins(window.utils.mapPins.children);
     }
   };
+
+
+  var pinOpen = function (evt) {
+    var targetPin = evt.target;
+    if (targetPin.parentNode.className === 'map__pin' || targetPin.className === 'map__pin') {
+      if (pinClicked) {
+        pinClicked.classList.remove('map__pin--active');
+      }
+      if (targetPin.parentNode.className === 'map__pin') {
+        pinClicked = targetPin.parentNode;
+      } else {
+        pinClicked = targetPin;
+      }
+      if (indexClickedPin !== window.map.findIndexPin(pinClicked, filteredPins)) {
+        indexClickedPin = window.map.findIndexPin(pinClicked, filteredPins);
+        pinClicked.classList.add('map__pin--active');
+        window.map.removeCard();
+        window.map.addCard(window.card.render, indexClickedPin);
+      } else {
+        evt.preventDefault();
+      }
+    }
+  };
+
   window.utils.turnBlocks(window.utils.advertFormBlocks, window.utils.disableBlock);
   window.utils.turnBlocks(window.utils.filterFormBlocks, window.utils.disableBlock);
   window.utils.advertAddress.value = window.form.formatMainPinAddress(window.utils.isMapOn);
   window.utils.mainPin.addEventListener('mousedown', function (evt) {
     window.utils.isClickEvent(evt, turnOnPage);
+
   });
 
   window.utils.mainPin.addEventListener('keydown', function (evt) {
     window.utils.isEnterEvent(evt, turnOnPage);
+  });
+
+  window.utils.mapPins.addEventListener('click', function (evt) {
+    pinOpen(evt);
+  });
+
+  window.utils.mapPins.addEventListener('keydown', function (evt) {
+    window.utils.isEnterEvent(evt, pinOpen);
   });
 
 })();
--- js/map.js
+++ js/map.js
@@ -2,6 +2,11 @@
 
 (function () {
   var filterContainer = window.utils.map.querySelector('.map__filters-container');
+  var addCard = function (renderFunction, advertIndex) {
+    var fragment = document.createDocumentFragment();
+    fragment.appendChild(renderFunction(window.data.adverts[advertIndex]));
+    window.utils.map.insertBefore(fragment, filterContainer);
+  };
   var addPins = function (renderFunction) {
     var fragment = document.createDocumentFragment();
     for (var i = 0; i < window.data.adverts.length; i++) {
@@ -10,21 +15,49 @@
     window.utils.mapPins.appendChild(fragment);
   };
 
-  var addCards = function (renderFunction) {
-    var fragment = document.createDocumentFragment();
-    fragment.appendChild(renderFunction(window.data.adverts[0]));
-    window.utils.map.insertBefore(fragment, filterContainer);
+  var removeCard = function () {
+    var cardPopup = window.utils.map.querySelector('.map__card.popup');
+    if (cardPopup) {
+      cardPopup.parentNode.removeChild(cardPopup);
+    }
   };
 
+  var onPopupEscPress = function (evt) {
+    if (evt.key === window.utils.ESCAPE_BUTTON) {
+      evt.preventDefault();
+      removeCard();
+    }
+  };
 
   var turnOnMap = function () {
     window.utils.map.classList.remove('map--faded');
     addPins(window.pin.render);
-
   };
+
+  var filterPins = function (collection) {
+    var filterChildren = [].slice.call(collection)
+      .filter(function (elem) {
+        return elem.className === 'map__pin';
+      });
+    return filterChildren;
+  };
+
+  var findIndexPin = function (pin, pins) {
+    for (var i = 0; i < pins.length; i++) {
+      if (pin === pins[i]) {
+        return i;
+      }
+    }
+    return null;
+  };
+
 
   window.map = {
     turnOn: turnOnMap,
-    addCards: addCards
+    addCard: addCard,
+    filterPins: filterPins,
+    findIndexPin: findIndexPin,
+    removeCard: removeCard,
+    onPopupEscPress: onPopupEscPress
   };
 })();
